9d0997f53f80f9da152f149b7476672c943332f8
钟世杰 <zhongshijie1995@outlook.com>
2022-06-21 09:31:23 +0800

    Initial commit

---
+++ src/main/java/com/zhongshijie1995/big_boot/BigBootApplication.java
@@ -0,0 +1,13 @@
+package com.zhongshijie1995.big_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhongshijie1995/big_boot/BigBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhongshijie1995.big_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
