d39d0f4fba7bf8b43dae3bcfa46d5384739434c1
钟世杰 <zhongshijie1995@outlook.com>
2022-07-18 18:51:39 +0800

    调整数据库

--- db/20220619/DDL.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset 钟世杰:1 labels:标准化项目1.0 context:所有分行
---comment: 标准化项目1.0建表脚本
-create table zsj (
-    zsj_id varchar(2),
-    zsj_name varchar(30)
-);
--- rollback drop table zsj;
--- db/20220619/DML.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset 钟世杰:1 labels:标准化项目1.0 context:所有分行
---comment: 标准化项目1.0数据铺底
-insert into zsj values ('1', 'zsj');
-insert into zsj values ('2', 'jsz');
--- rollback delete from zsj where zsj_id in ('1', '2');
