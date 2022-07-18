68ee59850c8dbd55ee366213ed485d8b8b9ba503
钟世杰 <zhongshijie1995@outlook.com>
2022-07-18 23:41:27 +0800

    增加项目部署测试RestHello

---
+++ src/main/java/com/zhongshijie1995/big_boot/test/TestController.java
@@ -0,0 +1,15 @@
+package com.zhongshijie1995.big_boot.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello() {
+        return "Hello";
+    }
+
+}
