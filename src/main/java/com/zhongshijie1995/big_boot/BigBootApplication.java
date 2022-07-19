0cd8f7a46582e7a1cb1f38126f83265afc970d6a
钟世杰 <zhongshijie1995@outlook.com>
2022-07-19 22:10:50 +0800

    适配Tomcat部署

--- src/main/java/com/zhongshijie1995/big_boot/BigBootApplication.java
+++ src/main/java/com/zhongshijie1995/big_boot/BigBootApplication.java
@@ -2,12 +2,18 @@ package com.zhongshijie1995.big_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BigBootApplication {
+public class BigBootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BigBootApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BigBootApplication.class);
+    }
 }
