5874b2865de2e9923e232cf402c5037edbeaf89d
钟世杰 <zhongshijie1995@outlook.com>
2022-08-02 18:55:05 +0800

    修改日志文件配置

--- src/main/java/com/zhongshijie1995/big_boot/util/log/AuditLog.java
+++ src/main/java/com/zhongshijie1995/big_boot/util/log/AuditLog.java
@@ -4,12 +4,12 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class AudiLogFilter extends Filter<ILoggingEvent> {
-    public static final String AUDI_TAG = "[AUDI]";
+public class AuditLog extends Filter<ILoggingEvent> {
+    public static final String AUDI_TAG = "[AUDIT]";
 
     @Override
     public FilterReply decide(ILoggingEvent iLoggingEvent) {
-        if (iLoggingEvent.getMessage().startsWith(AUDI_TAG)) {
+        if (iLoggingEvent.getFormattedMessage().startsWith(AUDI_TAG)) {
             return FilterReply.ACCEPT;
         }
         return FilterReply.DENY;
