e866d28cc411a83de187e52de7345d2af6413dbc
钟世杰 <zhongshijie1995@outlook.com>
2023-06-08 22:45:16 +0800

    解决数据库差异

--- src/main/java/com/zhongshijie1995/big_boot/info/mapper/VersionInfo.java
+++ src/main/java/com/zhongshijie1995/big_boot/info/mapper/VersionInfo.java
@@ -1,10 +1,12 @@
 package com.zhongshijie1995.big_boot.info.mapper;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class VersionInfo {
-    private int ids;
+    @TableId
+    private Integer ids;
     private String version;
     private String desc;
 }
